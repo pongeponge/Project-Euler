c39983dcb99dfd531e7e6243c76a50d5065683fb
pongeponge <sima8921@gmail.com>
2016-12-06 20:49:40 +0900

    Solved Problem31

--- Project Euler/Problem31/Program.cs
+++ Project Euler/Problem31/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,34 @@ namespace Problem31
 {
     class Program
     {
+        static int[] coins = { 2, 5, 10, 20, 50, 100, 200};
+        static int sum = 0;
+
         static void Main(string[] args)
         {
+            reco(200, coins.Length-1);
+            Debug.WriteLine(sum);
+        }
+
+        static Int64 CoinSums()
+        {
+            return 0;
+        }
+
+        static void reco(int zan, int c)
+        {
+            if (c < 0)
+            {
+                sum++;
+                return;
+            }
+
+            int n = zan / coins[c];
+
+            for (int i = 0; i <= n; i++)
+            {
+                reco(zan - i * coins[c], c - 1);
+            }
         }
     }
 }
