7c0833b3c96b63b5a6069ba669c0db3dbaf882b5
pongeponge <sima8921@gmail.com>
2016-12-08 16:36:12 +0900

    Solved Problem32

--- Project Euler/Problem32/Program.cs
+++ Project Euler/Problem32/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,62 @@ namespace Problem32
 {
     class Program
     {
+        int[] Numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
         static void Main(string[] args)
         {
+            Debug.WriteLine(PandigitalProducts());           
+        }
+
+        static Int64 PandigitalProducts()
+        {
+            List<int> answers = new List<int>();
+
+            CreateFormula(1,9,1234, 9876, answers);
+            CreateFormula(12,98,123, 987, answers);
+
+
+            return answers.Distinct().Sum();
+        }
+
+        static void CreateFormula(int amin, int amax, int bmin, int bmax, List<int> ans)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int a = amin; a <= amax; a++)
+            {
+                for (int b = bmin; b <= bmax; b++)
+                {
+                    int c = a * b;
+
+                    if (c.ToString().Length == 4)
+                    {
+                        sb.Append(a.ToString());
+                        sb.Append(b.ToString());
+                        sb.Append(c.ToString());
+
+                        if (CheckNumbers(sb.ToString().ToArray()) == true)
+                        {
+                            Debug.WriteLine(a + "*" + b + "=" + c);
+                            ans.Add(c);
+                        }
+                    }
+
+                    sb.Clear();
+                }
+            }
+        }
+
+        static bool CheckNumbers(char[] n)
+        {
+            Array.Sort(n);
+
+            for (int i = 1; i <= 9; i++)
+            {
+                if (n[i - 1] - 48 != i) return false;
+            }
+
+            return true;
         }
     }
 }
